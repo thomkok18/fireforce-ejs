845151d1d37efaa935646915dfdc474a1561cdfe
Thomk <thomkok13@hotmail.com>
2020-03-01 18:13:40 +0100

    authenticatie functies geïmporteerd. Als de user niet gevonden is tijdens inloggen, dan krijg je een error. Als niet alles is ingevoerd, dan krijg je een error. Meer gegevens van de gebruiker zitten in de session. Pagina's zijn alleen bereikbaar als je bent ingelogd of als je admin bent.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,9 @@
+const {auth, adminAuth} = require("../helpers/auth");
 const models = require("../models");
 const passwordHash = require('password-hash');
 
 exports.getLogin = (req, res, next) => {
-    res.render('users/login', {
-    });
+    return res.render('users/login', {});
 };
 
 exports.postCheckUser = (req, res, next) => {
@@ -11,15 +11,28 @@ exports.postCheckUser = (req, res, next) => {
     const pincode = req.body.pincode;
 
     models.User.findOne({ where: {email: email} }).then(user => {
+        if (!user) {
+            return res.render('users/login', {
+                error: "Email en/of pincode klopt niet.",
+            });
+        } else if (!email || !pincode) {
+            return res.render('users/login', {
+                error: "Vul alle gegevens in."
+            });
+        }
+
         if (passwordHash.verify(pincode, user.pincode)) {
             req.session.userObject = {
                 loggedIn: true,
                 id: user.id,
+                firstname: user.firstname,
+                inserts: user.inserts,
+                lastname: user.lastname,
+                email: user.email,
+                tel: user.tel,
                 roleId: user.roleId
             };
-            res.render('users/user-interface', {
-                email: user.email,
-            });
+            return res.redirect('/user-interface');
         }
     }).catch(err => {
         console.log(err);
@@ -28,13 +41,11 @@ exports.postCheckUser = (req, res, next) => {
 
 exports.getLogout = (req, res, next) => {
     req.session.userObject = null;
-    res.render('users/login', {
-    });
+    return res.redirect('/login');
 };
 
 exports.getRegister = (req, res, next) => {
-    res.render('users/register', {
-    });
+    return res.render('users/register', {});
 };
 
 exports.postStoreUser = (req, res, next) => {
@@ -44,6 +55,12 @@ exports.postStoreUser = (req, res, next) => {
     const email = req.body.email;
     const tel = req.body.tel;
     const pincode = passwordHash.generate(req.body.pincode);
+
+    if (!firstname || !lastname || !email || !tel || !pincode) {
+        return res.render('users/register', {
+            error: "Je hebt niet alle verplichte gegevens ingevoerd.",
+        });
+    }
 
     models.User.create({
         roleId: 2,
@@ -62,6 +79,13 @@ exports.postStoreUser = (req, res, next) => {
 };
 
 exports.getUserInterface = (req, res, next) => {
-    res.render('users/user-interface', {
+    auth(req, res);
+    return res.render('users/user-interface', {
+        email: req.session.userObject.email,
     });
+};
+
+exports.getAdminInterface = (req, res, next) => {
+    adminAuth(req, res);
+    return res.render('admin/index', {});
 };
